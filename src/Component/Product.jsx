2b0ee358632767d3c44272a070ee199abc2c9635
SATYAM-SRIVASTVA <satyam442001@gmail.com>
2022-05-27 17:21:30 +0530

    product clean

--- src/Component/Product.jsx
+++ src/Component/Product.jsx
@@ -4,21 +4,9 @@ import shampoo from './Assets/waterlesshampoo.png'
 
 const Product = () => {
   return (
-    <div className='product-cont'>
-      <div className="product-heading">
-        <h1>KOOKY PRODUCTS</h1>
-      </div>
-      <div className="first-row row">
-          <div className="column-1 col">
-             <div className="product-1">
-               <img src={shampoo} alt="" />
-             </div>
-          </div>
-          <div className="column-2 col">
-
-          </div>
-      </div>
-    </div>
+     <div>
+       
+     </div>
   )
 }
 
