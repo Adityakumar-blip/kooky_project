6bde1a42a85e8dda18955eba575857aef8ab535c
SATYAM-SRIVASTVA <satyam442001@gmail.com>
2022-05-27 17:00:51 +0530

    nav added

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,11 +1,48 @@
 import React from "react";
 import "./Navbar.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FiSearch } from "react-icons/fi";
+import { MdAccountCircle } from "react-icons/md";
 
 const Navbar = () => {
+   
   return (
-     <div className="nav-cont">
-        
-     </div>
+    <div className="nav-cont responsive">
+      <div className="nav-left">
+        <div className="hamburger" ><GiHamburgerMenu /></div>
+        <div className="account"><MdAccountCircle /></div>
+      </div>
+      <h1>
+        {" "}
+        <a href="" style={{ textDecoration: "none", color: "white" }}>
+          KOOKY
+        </a>
+      </h1>
+      <div className="nav-right">
+        <ul>
+          <li>
+            {" "}
+            <a href=""> Home</a>
+          </li>
+          <li>
+            {" "}
+            <a href="">Product</a>{" "}
+          </li>
+          <li>
+            {" "}
+            <a href="">How to use?</a>{" "}
+          </li>
+          <li>
+            {" "}
+            <a href="">Track your order</a>{" "}
+          </li>
+          <li>
+            {" "}
+            <a href="">About Us</a>{" "}
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
