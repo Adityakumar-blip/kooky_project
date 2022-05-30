d759e220a4a1fd19c39e97be638b9af198ec39d8
SHUBHAM GUPTA <gshubham804@gmail.com>
2022-05-31 02:01:49 +0530

    navbar updated

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -8,36 +8,23 @@ import {Link} from 'react-router-dom'
 const Navbar = () => {
    
   return (
-    <div className="nav-cont responsive">
-      <div className="nav-left">
-        <div className="hamburger" ><GiHamburgerMenu /></div>
-        <div className="account"><MdAccountCircle /></div>
-      </div>
-      <h1>
-        <a href="" style={{ textDecoration: "none", color: "white" }}>
-        <Link to="/"> KOOKY</Link>
-        </a>
-      </h1>
-      <div className="nav-right">
-        <ul>
-          <li>
-            <a href=""><Link to="/product">Product</Link></a>
-          </li>
-          <li>
-            <a href=""><Link to="/howto">How to</Link></a>
-          </li>
-          <li>
-            <a href=""><Link to="/track">Track</Link></a>
-          </li>
-          <li>
-            <a href=""><Link to="/stock">Investor</Link></a>
-          </li>
-          <li>
-            <a href=""><Link to="/about">About</Link></a>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <>
+    <nav>
+    <input type="checkbox" id="check"/>
+    <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+    </label>
+  <label class="logo"><Link to="/"> KOOKY</Link></label>
+  <ul>
+    <li><a class="active" href="#"><Link to="/product">Product</Link></a></li>
+    <li><a href="#"><Link to="/howto">How to</Link></a></li>
+    <li><a href="#"><Link to="/track">Track</Link></a></li>
+    <li><a href="#"><Link to="/stock">Investor</Link></a></li>
+    <li><a href="#"><Link to="/about">About</Link></a></li>
+  </ul>
+</nav>
+
+</>
   );
 };
 
